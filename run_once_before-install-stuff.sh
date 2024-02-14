dcfb0b714ca137496c3457669e12571b2ee49c89
Samuel Oliveira da Silva <samuel@pivotapp.ai>
2024-02-14 14:46:10 +0100

    adds prezto

--- run_once_before-install-stuff.sh
+++ run_once_before-install-stuff.sh
@@ -2,4 +2,6 @@
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl https://get.volta.sh | bash
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
 
