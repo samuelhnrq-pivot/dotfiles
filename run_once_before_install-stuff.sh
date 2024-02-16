2eb0553f0077f6933963c0256f8a704bc95f6715
Samuel Oliveira da Silva <samuel@pivotapp.ai>
2024-02-16 10:23:25 +0100

    zsh coreutils fixes

--- run_once_before_install-stuff.sh
+++ run_once_before_install-stuff.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/zsh
+#!/bin/zsh
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl https://get.volta.sh | bash
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+[[ -e ~/.zprezto ]] || git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
