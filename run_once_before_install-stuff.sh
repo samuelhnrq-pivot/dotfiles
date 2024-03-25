2b27920be2cebd46ad63ec30f28457a26084e03b
Samuel Oliveira da Silva <samuel@pivotapp.ai>
2024-03-25 13:38:31 +0100

    minor fixes and improvements

--- run_once_before_install-stuff.sh
+++ run_once_before_install-stuff.sh
@@ -2,5 +2,10 @@
 
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 curl https://get.volta.sh | bash
-[[ -e ~/.zprezto ]] || git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+[[ ! -e ~/.zprezto ]] && \
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+source "$(/opt/homebrew/bin/brew shellenv)"
+brew install oh-my-posh vim
+
 
